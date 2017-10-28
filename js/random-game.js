422cae1b3c3184472d25ff8ae61bdb2a3cdfed3e
amgranad <ambergkim@gmail.com>
2017-10-28 09:38:37 -0700

    fixed bug 4th asnwer bug

--- js/random-game.js
+++ js/random-game.js
@@ -17,7 +17,7 @@ console.log('Greeting the new user.');
 
 //random number guessing game.
 var randomNum = Math.floor(Math.random() * 10);
-var userAnswer1 = prompt('Guess a number from 0 to 10');//store user answer
+var userAnswer1 = prompt('Guess a number between 0 and 10');//store user answer
 var usersGuessDisplay = document.getElementById('scoreDisplay');
 var onPageGreeting = document.getElementById('greeting');
 var userAnswer1Tries = 4;//number of tries
@@ -25,10 +25,10 @@ console.log(user + ' answer is: ' + userAnswer1);
 
 var checkNumber = function() {
   if (isNaN(userAnswer1)){//checks if user answer is not a number
-    userAnswer1Tries = userAnswer1Tries - 1;
     alert('Please enter only numbers. You have ' + userAnswer1Tries + ' tries.');
     console.log('User Tries Left: ' + userAnswer1Tries + '. User answer is NaN.');
     userAnswer1 = prompt('Try again. How old am I? Please enter only numbers.');
+    userAnswer1Tries = userAnswer1Tries - 1;
   } else if (parseInt(userAnswer1) === randomNum) { //checks if user got the answer correctly
     alert('Yay! You got it!');
     usersGuessDisplay.innerHTML = 'You guessed the right number: ' + userAnswer1 + '.<br>Congratulations! You win!';//display congrats
@@ -37,25 +37,25 @@ var checkNumber = function() {
     userWin = true;
     randomNum = userAnswer1; // ends while loop early
   } else if (parseInt(userAnswer1) < randomNum){ //if answer is less than the random number
-    userAnswer1Tries = userAnswer1Tries - 1;
     alert('Your guess is low! You have ' + userAnswer1Tries + ' tries left.');
     console.log('User guessed low. User Tries Left: ' + userAnswer1Tries + '. User guessed: ' + userAnswer1 + ' vs. ' + randomNum + '.');
     userAnswer1 = prompt('Try again. Please enter only numbers.');
+    userAnswer1Tries = userAnswer1Tries - 1;
   } else {
-    userAnswer1Tries = userAnswer1Tries - 1;//if answer is greater than the random number
     alert('Your guess is high! You have ' + userAnswer1Tries + ' tries left.');
     console.log('User guessed high. User Tries Left: ' + userAnswer1Tries + '. User guessed: ' + userAnswer1 + ' vs. ' + randomNum + '.');
     userAnswer1 = prompt('Try again. Please enter only numbers.');
+    userAnswer1Tries = userAnswer1Tries - 1;//if answer is greater than the random number
   }
 };
 
-do {
-  checkNumber();
-} while ((userWin !== true) && (userAnswer1Tries > 1));
-
-if (userAnswer1Tries === 1) {
+if (userAnswer1Tries === 0) {
   usersGuessDisplay.innerHTML = 'Your last guess: ' + userAnswer1 + '.<br>The right answer: ' + randomNum + '.';
   onPageGreeting.innerHTML = 'Game Over.';
   alert('You ran out of tries. The correct answer was: ' + randomNum + '.\nGame Over.');
   console.log('User ran out of tries. Game over.');
 }
+
+do {
+  checkNumber();
+} while ((userWin !== true) && (userAnswer1Tries > -1));
